720b501da6a48a8dc86f49710b915b1ada0e851c
kneutron <50146127+kneutron@users.noreply.github.com>
2024-02-27 14:30:29 -0700

    Add files via upload

--- extractme.sh
+++ extractme.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+td=$PWD
+
+# xxx TODO EDITME if you have different storage - this is default directory
+cd /var/lib/vz/dump
+
 # REQUIRES: p7zip pkg
-time 7z x kingneutron-proxmox-lxc-container-gotify-v1-20240227.7z.001
+time 7z x $td/kingneutron-proxmox-lxc-container-gotify-v1-20240227.7z.001
